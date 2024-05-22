dcab0e3c0ed8ce54c8a963fb241c1f1ca64d3085
tr1pster <160784831+tr1pster@users.noreply.github.com>
2024-05-23 00:45:15 +0200

    cleaning

--- server/server.py
+++ server/server.py
@@ -10,20 +10,18 @@ ELECTION_RESULT_HASH = os.getenv("ELECTION_RESULT_HASH", "")
 
 @app.route('/verify_integrity', methods=['POST'])
 def verify_integrity():
-    data = request.json
-    data_hash = hashlib.sha256(json.dumps(data, sort_keys=True).encode()).hexdigest()
-
-    if data_hash == ELECTION_RESULT_HASH:
+    received_data = request.json
+    received_data_hash = hashlib.sha256(json.dumps(received_data, sort_keys=True).encode()).hexdigest()
+    if received_data_hash == ELECTION_RESULT_HASH:
         return jsonify({"message": "Election integrity verified", "status": "success"}), 200
     else:
         return jsonify({"message": "Election integrity compromised", "status": "failure"}), 403
 
 @app.route('/post_election_analytics', methods=['POST'])
 def post_election_analytics():
-    data = request.json
-
-    return jsonify({"message": "Post-election analytics processed", "data_received": data}), 200
+    analytics_data = request.json
+    return jsonify({"message": "Post-election analytics processed", "data_received": analytics_data}), 200
 
 if __name__ == '__main__':
     FLASK_RUN_PORT = os.getenv("FLASK_RUN_PORT", 5000)
-    app.run(debug=True, port=FLASK_RUN_PORT)
+    app.run(debug=True, port=int(FLASK_RUN_PORT))
