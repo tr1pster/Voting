79e0700e2ac5d4e177826256a15464a483749c07
tr1pster <160784831+tr1pster@users.noreply.github.com>
2024-05-24 03:42:49 +0200

    add optimization

--- server/server.py
+++ server/server.py
@@ -2,16 +2,21 @@ from flask import Flask, request, jsonify
 import os
 import hashlib
 import json
+from functools import lru_cache
 
 app = Flask(__name__)
 
 SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key_here")
 ELECTION_RESULT_HASH = os.getenv("ELECTION_RESULT_HASH", "")
 
+@lru_cache(maxsize=None)  # Caching with unlimited size, consider setting a size limit.
+def hash_data(data):
+    return hashlib.sha256(json.dumps(data, sort_keys=True).encode()).hexdigest()
+
 @app.route('/verify_integrity', methods=['POST'])
 def verify_integrity():
     received_data = request.json
-    received_data_hash = hashlib.sha256(json.dumps(received_data, sort_keys=True).encode()).hexdigest()
+    received_data_hash = hash_data(received_data)
     if received_data_hash == ELECTION_RESULT_HASH:
         return jsonify({"message": "Election integrity verified", "status": "success"}), 200
     else:
